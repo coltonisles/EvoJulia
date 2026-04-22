e648870f8712ff71084588ab3e8782ae8453570e
David Mulligan <t00745894@mytru.ca>
2026-04-21 17:07:38 -0700

    oh jeez it was just an abandoned attempt to clean up commented code, phew

--- backend/evolution.py
+++ backend/evolution.py
@@ -2,12 +2,6 @@
 Run the GA for EACH tile
 
 genome: [fractal_id, crop_cx, crop_cy, crop_scale, brightness_shift]
-
-  - fractal_id:         discrete (for crossover / mutation)
-  - crop_cx:            continuous (0-1)
-  - crop_cy:            continuous (0-1)
-  - crop_scale:         continuous (min - max))
-  - brightness_shift:   continuous (-0.4 - 0.4)
 """
 from config import GAConfig as ga, MosaicConfig as mosaic, SampleFractalConfig
 import numpy as np
